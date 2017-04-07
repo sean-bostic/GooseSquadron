755f6a6490df50e8c257b5a0852d55ba96bd7df6
Sean Bostic <sean.bostic@gmail.com>
2017-04-06 20:04:08 -0400

    Initial Commit
    Initial commit. Needs a lot of rework.

---
+++ sketch.js
@@ -0,0 +1,97 @@
+//Variables
+var bird;
+
+function setup(){
+  createCanvas(400, 600);
+  bird = new Bird();
+}
+
+function draw(){
+  background(0);
+
+  bird.update();
+  bird.show();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var bird;
+// var bird2;
+// // var pipes = [];
+// var score = 0;
+//
+// function setup() {
+//   createCanvas(400, 600);
+//   bird = new Bird();
+//   bird2 = new Bird();
+//   // pipes.push(new Pipe());
+// }
+//
+// function draw() {
+//   background(0);
+//
+//   // for (var i = pipes.length-1; i >= 0; i--) {
+//   //   pipes[i].show();
+//   //   pipes[i].update();
+//   //
+//   //   //make logic when each pipe is hit by the bird?
+//   //   if (pipes[i].hits(bird)) {
+//   //     score = score - 1;
+//   //     endGame();
+//   //   }
+//
+//   }
+//
+//   bird.update();
+//   bird.show();
+//   //Daniel Shiffman figured this part out
+//   // if (frameCount % 100 == 0) {
+//   // pipes.push(new Pipe())
+//   //
+//   // }
+// }
+//
+// function endGame(){
+//   console.log("End the game here");
+//   console.log(score);
+//   bird.x = 0;
+//   bird.speed = 0;
+//   pipes[i].speed = 0;
+//   pipes[i].x = 0;
+// }
+//
+// function keyPressed() {
+//   if (key == ' ') {
+//     bird.up();
+//   }
+// }
